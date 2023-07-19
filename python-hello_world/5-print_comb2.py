867d33e0c156a0eec48daf79e7a30f5b7a0ea32c
eyoba <eyobabebe28@gmail.com>
2023-07-19 15:01:43 -0500

    fifth

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -2,6 +2,5 @@ for i in range(100):
     if i<10:
         print("0{}, ".format(i), end="" )
     else:
-        print("{}, ".format(i), end="" )
-    if (i + 1) % 10 == 0:
-            print()
+        print("{}, ".format(i), end="," )
+    
