af9124541a165a7283d08ce76d1d14ef66b8ecbc
eyoba <eyobabebe28@gmail.com>
2023-07-27 09:47:20 -0500

    module 6

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,5 +1,5 @@
 def raise_exception_msg(message=""):
     try:
-        raise NameError('C is fun')
-    except NameError as ne:
-        print("python is cool")
+        raise TypeError(message)
+    except TypeError as ne:
+        print(ne)
