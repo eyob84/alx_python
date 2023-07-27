fbacb7808cc60cc72f4852623706184b466e9dc0
eyoba <eyobabebe28@gmail.com>
2023-07-27 08:50:48 -0500

    module 2

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -5,12 +5,12 @@ def ff(*argv):
     if len(argv[0])==0:
         print("0 arguments.") 
     elif len(argv[0])==1:
-        print("1 argument.")
+        print("1 argument:")
         print("1: {}".format(argv[0][1]))
     else:
         x = argv[0]
         # print(x)
-        print("{}: arguments".format(len(argv[0])-1))
+        print("{}: arguments:".format(len(argv[0])-1))
         for i in range(1, len(x) ):
             print("{}: {}".format(i,x[i]))
 if __name__ == "__main__":
