85eb3c3c26bb020e00c2ad4dc322b16b79238f4c
eyoba <eyobabebe28@gmail.com>
2023-08-10 07:40:42 -0500

    base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -3,10 +3,10 @@ class Base():
     def __init__(self, id=None):
             """this is a base class """
             if id:
-                  self.id = id
+                self.id = id
             else:
-                 __nb_objects +=1  
-                 self.id=Base.__nb_objects
+                __class__.__nb_objects +=1  
+                self.id=__class__.__nb_objects
     __doc__ = """
     this is documentation for my class
     """
