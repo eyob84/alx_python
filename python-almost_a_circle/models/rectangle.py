a0c34ba03e53d490aee859abdc229f98b4be86ed
eyoba <eyobabebe28@gmail.com>
2023-08-10 08:15:10 -0500

    afa

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,16 @@
+class Base():
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """sadfafa """
+        super().__init__(id)
+        self.__width=width
+        self.__height=height
+        self.__y=y
+        self.__x=x
+    __doc__ = """
+    this is documentation for my class
+    """
+    
+
+__doc__ = """
+this is documentation for my module
+"""
