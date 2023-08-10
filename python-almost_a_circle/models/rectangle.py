6a26b4087209a8665b0d6a7149c20bde468299c8
eyoba <eyobabebe28@gmail.com>
2023-08-10 08:18:20 -0500

    sdasf

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,5 @@
 from models.base import Base
-class Rectngle(Base):
+class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """sadfafa """
         super().__init__(id)
