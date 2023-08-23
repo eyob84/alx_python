aef57ff2d3f3959a1869097b416bc7306e637f3f
eyoba <eyobabebe28@gmail.com>
2023-08-23 08:26:54 -0500

    filter

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,37 @@
+import MySQLdb
+import sys
+
+
+def search_states(username, password, database):
+    try:
+    
+        conn = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
+
+        cursor = conn.cursor()
+        query = """ SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC; """
+        cursor.execute(query)
+        
+        rows = cursor.fetchall()
+        if rows:
+            for row in rows:
+                print(row)
+        else:
+            # print()
+            pass
+        cursor.close()
+        conn.close()
+
+    except MySQLdb.Error as e:
+        print("Error connecting to MySQL:", e)
+        sys.exit(1)
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    search_states(username, password, database)
