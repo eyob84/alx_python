6b6efc0affda95455cd555ef77a1be8da0bd528f
eyoba <eyobabebe28@gmail.com>
2023-08-23 08:18:06 -0500

    connection

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,41 +1,46 @@
-import MySQLdb
 import sys
+import MySQLdb
 
 
-def states(username, password, database):
-    try:
-        conn = MySQLdb.connect(
-                host='localhost',
-                port=3306,
-                user=username,
-                passwd=password,
-                db=database
-            )
-        cursor = conn.cursor()
-        query ="""SELECT * FROM states"""
-        cursor.execute(query)
-        rows = cursor.fetchall()
+# import sqlalchemy
+# import pymysql
+# pymysql.install_as_MySQLdb()
+# import pymysql as MySQLdb
+# import MySQLdb import _mysql
+# from MySQLdb.constants import FIELD_TYPE
+# Define the database connection details
+# username = sys.argv[1]
+# password = sys.argv[2]
+# database = sys.argv[3]
+# my_conv = { FIELD_TYPE.LONG: int }
+# db=_mysql.connect(conv=my_conv,host="localhost",user=username,password=password,database=database)
+# r=db.store_result()
+# # ...or...
+# # r=db.use_result()
+# r.fetch_row(maxrows=0)
+# print(MySQLdb.__version__)
+# print(sqlalchemy.__version__)
+def dostg():
+    # Open database connection
+    u = sys.argv[1]
+    p = sys.argv[2]
+    d = sys.argv[3]
+    db = MySQLdb.connect("localhost", u, p, d, 3306)
 
-        if rows:
-            for row in rows:
-             print(row)
-        else:
-            # print()
-            pass
-        cursor.close()
-        conn.close()
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
 
-    except MySQLdb.Error as e:
-        print("Error connecting to MySQL:", e)
-        sys.exit(1)
+    # execute SQL query using execute() method.
+    cursor.execute("SELECT * FROM states  ORDER BY id ASC;")
+
+    # Fetch a single row using fetchone() method.
+    data = cursor.fetchall()
+    for i in data:
+        print(i, sep="\n")
+
+    # disconnect from server
+    db.close()
+
+
 if __name__ == "__main__":
-   
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-   
-    states(username, password, database)
-    
-
-
-
+    dostg()
