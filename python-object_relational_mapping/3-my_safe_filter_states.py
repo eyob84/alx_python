3db0c696c96b536889f208cb90bb5b3699f36dc9
eyoba <eyobabebe28@gmail.com>
2023-08-23 09:01:50 -0500

    no sql injection

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -25,8 +25,8 @@ def dostg():
     u = sys.argv[1]
     p = sys.argv[2]
     d = sys.argv[3]
-    e = sys.argv[4]
-    db = MySQLdb.connect("localhost", u, p, d, e, 3306)
+   
+    db = MySQLdb.connect("localhost", u, p, d, 3306)
 
     # prepare a cursor object using cursor() method
     cursor = db.cursor()
