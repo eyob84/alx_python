ea26050f34fda1bdf60bd4853444029ef1e93a32
eyoba <eyobabebe28@gmail.com>
2023-08-23 09:54:47 -0500

    filter citites

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -13,19 +13,27 @@ def search_states(username, password, database, state_name):
             db=database
         )
 
-        
+        # Create a cursor object to execute SQL queries
         cursor = conn.cursor()
 
         # Prepare the SQL query with a parameterized query
-        query = """ SELECT cities.name FROM cities
-        JOIN states ON  cities.state_id = states.id  WHERE states.name = %s ORDER BY cities.id ASC; """
+        query = """SELECT  cities.name FROM states
+        INNER JOIN cities ON
+        cities.state_id = states.id WHERE states.name = %s
+        ORDER BY cities.id ASC; """
+
+        # Execute the query with the state name as a parameter
         cursor.execute(query, (state_name,))
+
+        # Fetch all the rows returned by the query
         rows = cursor.fetchall()
+
+        # Display the results
         if rows:
-            for row in rows:
-                print(row)
+            for i, row in enumerate(rows):
+                print(row, end=', ' if i < len(rows)-1 else '\n')
         else:
-            pass
+            print()
 
         # Close the cursor and connection
         cursor.close()
