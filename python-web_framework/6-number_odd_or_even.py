7eb10e32c419af83b35340cde840ced50faf7488
eyoba <eyobabebe28@gmail.com>
2023-08-31 07:47:08 -0500

    templates

--- python-web_framework/6-number_odd_or_even.py
+++ python-web_framework/6-number_odd_or_even.py
@@ -27,6 +27,11 @@ def number(n):
 def number_template(n):
     return render_template('number_template.html', number=n)
 
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    even_or_odd = "even" if n % 2 == 0 else "odd"
+    return render_template('number_odd_or_even.html', number=n, even_or_odd=even_or_odd)
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
 
